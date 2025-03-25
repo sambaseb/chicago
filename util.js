ce768f53d6c27786e88db7605122d9564f7f9ccb
sambaseb <68056962+sambaseb@users.noreply.github.com>
2025-03-25 19:46:53 +0100

    zoom

--- util.js
+++
@@ -1,9 +0,0 @@
-let lastTouchEnd = 0;
-
-document.addEventListener('touchend', function (event) {
-  const now = new Date().getTime();
-  if (now - lastTouchEnd <= 300) {
-    event.preventDefault(); // Block double-tap zoom
-  }
-  lastTouchEnd = now;
-}, false);
